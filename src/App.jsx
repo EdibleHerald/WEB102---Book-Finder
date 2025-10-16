3c63ea7a8597a58ecc8ca55dade5d297e78621c5
EdibleHerald <142949302+EdibleHerald@users.noreply.github.com>
2025-10-16 18:07:14 -0400

    Added a basic webpage setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+const ACCESS_KEY = import.meta.env.VITE_ACCESS_KEY;
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   
+    <div className="wholePage">
+      {/*Plan: 1 row / 3 column grid with gap.*/}
+      <div className="prevBooks">
+        <h4>Previous books:</h4> 
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+      <div className="mainContent">
+        <h1>Book Finder</h1>
+        <h3>Find your next book here!</h3>
+        {/* Include component here */}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+      <div className="banList">
+        <div>
+          <h2>Ban List</h2>
+          <p>Attributes here are banned! Click them to unban them.</p>
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
